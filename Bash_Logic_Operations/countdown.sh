6ef55b2c2e5514c38df95f8e25c9fdf9032e33af
naomikortey75 <naomikortey0@gmail.com>
2024-10-21 16:42:40 +0000

    question

---
+++ Bash_Logic_Operations/countdown.sh
@@ -0,0 +1,10 @@
+while true; do
+   echo "Enter a number or type 'q' to quit"
+    read number
+
+   if [[ "$number" == "q" ]]; then
+    break
+   fi
+    echo "You entered $number"
+done
+echo "Existing the loop"
