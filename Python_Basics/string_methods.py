e97d30577fc9d817c3dfb46f4a7ab2bba0821a63
naomikortey75 <naomikortey0@gmail.com>
2024-10-23 14:25:46 +0000

    Question 3

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#message = 'Hello, my name is Naomi'
+#print(message.upper())
+#print(message.lower())
+#print(message.replace('Naomi', 'Mercy'))
+
+name = 'Naomi'
+greeting = 'Good Afternoon,'
+print(greeting + ' ' + name)
