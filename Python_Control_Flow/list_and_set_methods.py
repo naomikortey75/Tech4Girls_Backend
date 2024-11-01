12ae730aed75619aab3c3eb70ef9a2c20d1b6f86
naomikortey75 <naomikortey0@gmail.com>
2024-11-01 13:55:46 +0000

    Question 2

---
+++ Python_Control_Flow/list_and_set_methods.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""my_pets = ['dog', 'cat', 'dolphin']
+print(my_pets)
+my_toys = ['teddy', 'car', 'micky']
+print(my_toys)
+my_pets.append('monkey')
+print(my_pets)
+
+my_pets.remove('monkey')
+print(my_pets)
+
+my_pets.pop()
+print(my_pets)
+
+my_pets.sort()
+print(my_pets)
+
+my_pets.reverse()
+print(my_pets)
+
+print(my_pets.extend(my_toys))"""
+
+
+#ii
+Odd = {3, 5, 7, 9}
+Even = {4, 6, 8, 10}
+print(Odd)
+
+Odd.add(4)
+print(Odd)
+
+Odd.remove(3)
+print(Odd)
+
+Odd.union(Even)
+print(Odd)
+
+Odd.intersection(Even)
+print(Odd)
+
+Odd.difference(Even)
+print(Odd)
